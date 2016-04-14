5d5140675e659ddc9012dfb0a84a8fd0f8dc4208
Karthik Jain <karthikjain@gmail.com>
2016-04-14 13:58:18 -0700

    removed files

--- src/twitter_twitterAPI.py
+++
@@ -1,9 +0,0 @@
-from TwitterAPI import TwitterAPI
- 
-consumer_key =   "cZUYhg1AGiFeERJyk1yEgE5oI"
-consumer_secret = "JHkGyWALQNe1avsRYJovsUwqzCY0nWCtG9nFrq0JuNmKbMs0u6"
-access_token_key = "178344419-BpArn9mWvIgRd0m0wZ83iI52BzgLjR7IVgBCR6Yg"
-access_token_secret = "Yj6jijQWx4FWYw6TAhJDEi9lwTbdrcidUNBOBh3IwuR6l"
-
-api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
- 
