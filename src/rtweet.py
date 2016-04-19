19ea5214eb6b9ead55fdc59735ac9778eeec40da
Karthik Jain <karthikjain@gmail.com>
2016-04-18 17:26:07 -0700

    random functions

--- src/rtweet.py
+++ src/rtweet.py
@@ -23,6 +23,15 @@
 
 import sys
 import requests, json, urllib, urllib2, base64
+import random
+
+def get_state():
+    state_list = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+    return random.choice(state_list)
+
+
+def get_age():
+    return random.randint(18,60)
 
 def read(filename):
     searchList = []
