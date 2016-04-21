6b0298056791b62913b04c820af41554a9b18e6a
Karthik Jain <karthikjain@gmail.com>
2016-04-20 19:20:06 -0700

    adding credentials

--- src/credentials.py
+++ src/credentials.py
@@ -1,2 +1,4 @@
 twitter_consumer_key='TWITTER_CONSUMER_KEY' 
 twitter_consumer_secret='TWITTER_CONSUMER_SECRET'
+twitter_access_token='TWITTER_CONSUMER_SECRET'
+twitter_access_token_secret='TWITTER_CONSUMER_SECRET'
--- src/stweet.py
+++ src/stweet.py
@@ -1,27 +1,55 @@
 #Import the necessary methods from tweepy library
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
-
-#Variables that contains the user credentials to access Twitter API 
-access_token = "178344419-BpArn9mWvIgRd0m0wZ83iI52BzgLjR7IVgBCR6Yg"
-access_token_secret = "Yj6jijQWx4FWYw6TAhJDEi9lwTbdrcidUNBOBh3IwuR6l"
-consumer_key = "cZUYhg1AGiFeERJyk1yEgE5oI"
-consumer_secret = "JHkGyWALQNe1avsRYJovsUwqzCY0nWCtG9nFrq0JuNmKbMs0u6"
-
+#from tweepy.streaming import StreamListener
+#from tweepy import OAuthHandler
+#from tweepy import Stream
+import json
+import tweepy
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
         print ( data )
+        for line in data:
+            try:
+                tweets.append(json.loads(line))
+            except:
+                pass
+        
         return True
 
     def on_error(self, status):
         print ( status )
 
+def get_credentials():
+    # read twitter API from file
+    api_keys = {}
+    api_keys['twitter_consumer_key']    = ''
+    api_keys['twitter_consumer_secret'] = ''
+    api_keys['twitter_access_token_secret'] = ''
+    api_keys['twitter_access_token'] = ''
 
-if __name__ == '__main__':
+    try:
+        import credentials
+        api_keys['twitter_consumer_key']    = credentials.twitter_consumer_key
+        api_keys['twitter_consumer_secret'] = credentials.twitter_consumer_secret
+        api_keys['twitter_access_token'] = credentials.twitter_access_token
+        api_keys['twitter_access_token_secret'] = credentials.twitter_access_token_secret
+    except ImportError:
+        print("rtweet.py: ALERT: No credentials.py found")
+        api_keys['twitter_consumer_key']    = raw_input("Enter your Twitter API consumer key: ")
+        api_keys['twitter_consumer_secret'] = raw_input("Enter your Twitter API consumer secret: ")
+        api_keys['twitter_access_token'] = raw_input("Enter your Twitter API access token: ")
+        api_keys['twitter_access_token_secret'] = raw_input("Enter your Twitter API access token secret: ")
+    except:
+        print("rtweet.py: ERROR: unable to import credentials - unknown reason")
+
+    return api_keys
+
+def main ()
+    
+    # read API Keys
+    api_keys = get_credentials()
 
     #This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
@@ -31,4 +59,9 @@ if __name__ == '__main__':
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
     stream.filter(track=['python', 'javascript', 'ruby'])
+ 
+    return
+
+if __name__ == '__main__':
+    main ()
 
